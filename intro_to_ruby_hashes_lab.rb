f948a3676efca6652ab26ebd498611a67204c246
Samuel Lesser <ineedmoreplates@gmail.com>
2020-02-17 21:42:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,10 +35,10 @@ def update_counting_hash(hash, key)
   
     if hash[key]
       hash[key] += 1
-      hash => key
+      key => value
           else
       hash[key] = 1
-      hash => key
+      key => value
     end
     
 end
